f640558a1f8d6721d4ecf1a262a0589787699540
Jonelle English <jmenglish216@gmail.com>
2018-08-03 20:54:16 -0400

    Assignment 8 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#album-artist').text(album.artist);
+  $('#album-releaseInfo').text(album.releaseInfo);
 }
