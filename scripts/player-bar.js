17981d34bb7c72ee8a56ab6e0cc23c9f12a8fd11
Jonelle English <jmenglish216@gmail.com>
2018-08-10 16:39:24 -0400

    final revisions

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,19 +4,6 @@
     $(this).attr('playState', player.playState);
   });
 
-  $('button#next').on('click', function() {
-    if (player.playState !== "playing") { return; }
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-  });
-
-}
-
    $('button#next').on('click', function() {
     if (player.playState !== "playing") { return; }
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -52,13 +39,6 @@
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-
-    $('#time-control .current-time').text( currentTime );
-    $('#time-control input').val(percent);
-  }, 1000);
-
- }
-
     $('#time-control .current-time').text( currentTime);
     $('#time-control input').val(percent);
     $('#time-control .total-time').text( duration);
