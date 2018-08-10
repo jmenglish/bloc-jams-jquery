123031fabb210e6fb5e312766a45cbca77299f78
Jonelle English <jmenglish216@gmail.com>
2018-08-10 16:25:22 -0400

    final reivions

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,9 +3,6 @@
     player.playPause();
     $(this).attr('playState', player.playState);
   });
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   $('button#next').on('click', function() {
     if (player.playState !== "playing") { return; }
@@ -19,11 +16,7 @@
   });
 
 }
-=======
-=======
->>>>>>> checkpoint-11-blocjams
-=======
->>>>>>> assignment-11-blocjams
+
    $('button#next').on('click', function() {
     if (player.playState !== "playing") { return; }
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -32,10 +25,7 @@
      const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
-<<<<<<< HEAD
-=======
 
->>>>>>> assignment-11-blocjams
   $('button#previous').on('click', function() {
    if (player.playState !== "playing") { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -44,39 +34,31 @@
     const nextSong = album.songs[nextSongIndex];
    player.playPause(nextSong);
  });
-<<<<<<< HEAD
-<<<<<<< HEAD
- }
->>>>>>> assignment-10-blocjams
-=======
-=======
->>>>>>> assignment-11-blocjams
+
 
   $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
   });
 
-<<<<<<< HEAD
-=======
+
 
   $('#volume-control input').on('input', function (event) {
       player.setVolume(event.target.value);
   });
 
->>>>>>> assignment-11-blocjams
+
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-<<<<<<< HEAD
+
     $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
 
  }
->>>>>>> checkpoint-11-blocjams
-=======
+
     $('#time-control .current-time').text( currentTime);
     $('#time-control input').val(percent);
     $('#time-control .total-time').text( duration);
@@ -84,4 +66,3 @@
 
 
  }
->>>>>>> assignment-11-blocjams
--- scripts/song-list.js
+++ scripts/song-list.js
@@ -13,37 +13,19 @@
         <td>${song.duration}</td>
       </tr>
     `);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
     song.element.on('click', event => {
       player.playPause(song);
     });
 
     $('#song-list').append(song.element);
-=======
-=======
->>>>>>> assignment-10-blocjams
-=======
->>>>>>> checkpoint-11-blocjams
-=======
->>>>>>> assignment-11-blocjams
+
      song.element.on('click', event => {
       player.playPause(song);
       $('button#play-pause').attr('playState', player.playState);
     });
      $('#song-list').append(song.element);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> checkpoint-10-blocjams
-=======
->>>>>>> assignment-10-blocjams
-=======
->>>>>>> checkpoint-11-blocjams
-=======
->>>>>>> assignment-11-blocjams
+
   });
 }
